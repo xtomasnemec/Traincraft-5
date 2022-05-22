e4117b4f2ba3ac346c686e3d83210d7777ad420a
Jonathan <jakedude435@live.com>
2022-05-22 13:18:03 -0500

    block render position fix

--- src/main/java/ebf/tim/render/BlockTESR.java
+++ src/main/java/ebf/tim/render/BlockTESR.java
@@ -8,9 +8,11 @@ public class BlockTESR extends TileEntitySpecialRenderer {
 
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float tick) {
+        GL11.glPushMatrix();
         if(tileEntity.getWorldObj()!=null) {
             GL11.glTranslated(x, y, z);
         }
         tileEntity.func_145828_a(null);
+        GL11.glPopMatrix();
     }
 } 
