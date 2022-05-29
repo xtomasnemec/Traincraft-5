52a921e5701f36a83cb3e16538c4f8274bc1c69e
Jonathan <jakedude435@live.com>
2022-05-29 18:43:37 -0500

    small crashfixes
    - Fixed a crash with some wrong particle data in the kof
    - Fixed a crash with the bridge pillar (it actually doesn't crash 1.7, just 1.12, but it's wrong in both either way)

--- src/main/java/train/blocks/BlockBridgePillar.java
+++ src/main/java/train/blocks/BlockBridgePillar.java
@@ -58,32 +58,7 @@ public class BlockBridgePillar extends BlockDynamic {
 		return new tilePillar(this);
 	}
 	
-	/**
-	 * Called when the block is placed in the world.
-	 */
-	@Override
-	public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLiving, ItemStack par6ItemStack) {
-		int l = CommonUtil.floorDouble((double) (par5EntityLiving.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-		int i1 = CommonUtil.getBlockFacing(par1World,par2, par3, par4) >> 2;
-		++l;
-		l %= 4;
 
-		if (l == 0) {
-			CommonUtil.setBlockMeta(par1World,par2, par3, par4, 2 | i1 << 2);
-		}
-
-		if (l == 1) {
-			CommonUtil.setBlockMeta(par1World,par2, par3, par4, 3 | i1 << 2);
-		}
-
-		if (l == 2) {
-			CommonUtil.setBlockMeta(par1World,par2, par3, par4, 0 | i1 << 2);
-		}
-
-		if (l == 3) {
-			CommonUtil.setBlockMeta(par1World,par2, par3, par4, 1 | i1 << 2);
-		}
-	}
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister iconRegister) {
